7b58bc2ff30a3d6b3db6bfbacc2089a1b42dc99c
Jacob <wittenberg.jacob@gmail.com>
2020-01-04 12:25:56 -0600

    switching branch for auth

--- src/App/App.js
+++ src/App/App.js
@@ -20,7 +20,7 @@ const PrivateRoute = ({ component: Component, authed, ...rest }) => {
 
 class App extends React.Component {
   state = {
-    authed: false,
+    authed: true,
   }
 
   render() {
--- src/Components/pages/SingleBoard/SingleBoard.js
+++ src/Components/pages/SingleBoard/SingleBoard.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './SingleBoard.scss';
-
+// the function is finding the id correctly, but it's not printing. swing back around on it.
 class SingleBoard extends React.Component {
   render() {
+    const { boardId } = this.props.match.params.boardId;
     return (
       <div className="SingleBoard">
         <h1>Single Board Page</h1>
+        <h2>Current Board Id is {boardId}</h2>
       </div>
     );
   }
