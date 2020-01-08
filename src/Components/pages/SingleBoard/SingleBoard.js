f4a3a955b479d37f6243285946deb0978f187b2b
Jacob <wittenberg.jacob@gmail.com>
2020-01-07 20:27:14 -0600

    pins printing

--- src/Components/pages/SingleBoard/SingleBoard.js
+++ src/Components/pages/SingleBoard/SingleBoard.js
@@ -1,13 +1,41 @@
 import React from 'react';
 import './SingleBoard.scss';
-// the function is finding the id correctly, but it's not printing. swing back around on it.
+import boardData from '../../../helpers/data/boardData';
+import pinData from '../../../helpers/data/pinData';
+
+import Pin from '../../shared/Pin/Pin';
+
 class SingleBoard extends React.Component {
+  state = {
+    board: {},
+    pins: [],
+  }
+
+  getPinData = (boardId) => {
+    pinData.getPinsByBoardId(boardId)
+      .then((pins) => this.setState({ pins }))
+      .catch((error) => console.error('error from getpindata', error));
+  }
+
+  componentDidMount() {
+    const { boardId } = this.props.match.params;
+    boardData.getSingleBoard(boardId)
+      .then((response) => {
+        this.setState({ board: response.data });
+        this.getPinData(boardId);
+      })
+      .catch((error) => console.error('error from getSingleBoard', error));
+  }
+
   render() {
     const { boardId } = this.props.match.params;
     return (
       <div className="SingleBoard">
         <h1>Single Board Page</h1>
         <h2>Current Board Id is {boardId}</h2>
+        <div className="pins d-flex flex-wrap">
+          { this.state.pins.map((pin) => <Pin key={pin.id} pin={pin}/>)}
+        </div>
       </div>
     );
   }
--- src/Components/shared/Board/Board.js
+++ src/Components/shared/Board/Board.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './Board.scss';
 import boardShape from '../../../helpers/propz/boardShape';
@@ -16,6 +17,7 @@ class Board extends React.Component {
           <div className="card-body">
             <h5 className="card-title">{board.name}</h5>
             <p className="card-text">{board.description}</p>
+            <Link className="btn btn-primary" to={`/board/${board.id}`}>View Board</Link>
           </div>
         </div>
       </div>
